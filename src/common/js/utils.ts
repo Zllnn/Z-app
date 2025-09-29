8f4fc5b92e81d420fe9c9983931d8754c2b946fc
Zllnn <3247004241@qq.com>
2025-09-29 20:56:44 +0800

    chore：删除不必要的注释

--- src/common/js/utils.ts
+++ src/common/js/utils.ts
@@ -164,12 +164,4 @@ export const tdist: {
   }
 }
 
-// 图片前缀方法
-export const prefix = (url: string): string => {
-  if (url && url.startsWith('http')) {
-    return url
-  } else {
-    url = `http://backend-api-01.newbee.ltd${url}`
-    return url
-  }
-}
+
